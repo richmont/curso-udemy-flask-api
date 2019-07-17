d69bf60ff686b8b4398425de8243d15f8f0c1605
Richelmy Monteiro <richelmy.monteiro@gmail.com>
2019-07-16 23:59:01 -0300

    Primeira versão com CRUD baseado em memória

---
+++ models/HotelModel.py
@@ -0,0 +1,17 @@
+class HotelModel:
+    # construtor do modelo de objeto
+    def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
+        self.hotel_id = hotel_id
+        self.nome = nome
+        self.estrelas = estrelas
+        self.diaria = diaria
+        self.cidade = cidade
+    def json(self):
+        # converte objeto recem criado para formato json
+        return {
+            'hotel_id': self.hotel_id,
+            'nome': self.nome,
+            'estrelas': self.estrelas,
+            'diaria': self.diaria,
+            'cidade': self.cidade
+        }
